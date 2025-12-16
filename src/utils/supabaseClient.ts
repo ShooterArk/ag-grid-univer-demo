b4e1a39a395f84672f5c3dc3830cd9a4c0667078
Yawar Shah <yawar@deltacrontech.com>
2025-12-17 00:42:47 +0500

    fix env key

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js'
 
 // Vite uses import.meta.env instead of process.env
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY as string
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
